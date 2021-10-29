162b7f2366d31d84d6dd65bab9e418383e802561
microdust <wuilliamolgo104286@gmail.com>
2021-10-29 15:27:44 +0800

    update

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,29 +1,29 @@
-self.addEventListener('install', function (event) {
+self.addEventListener('install', function(event) {
     console.log('[SW] 安裝(Install) Service Worker!', event);
     self.skipWaiting();
 
     var offlinePage = new Request('offline.html');
     event.waitUntil(
-        fetch(offlinePage).then(function (response) {
-            return caches.open('offline2').then(function (cache) {
+        fetch(offlinePage).then(function(response) {
+            return caches.open('offline2').then(function(cache) {
                 return cache.put(offlinePage, response);
             });
         }));
 });
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-        fetch(event.request).catch(function (error) {
-            return caches.open('offline2').then(function (cache) {
+        fetch(event.request).catch(function(error) {
+            return caches.open('offline2').then(function(cache) {
                 return cache.match('offline.html');
             });
         }));
 });
-self.addEventListener('refreshOffline', function (response) {
-    return caches.open('offline2').then(function (cache) {
+self.addEventListener('refreshOffline', function(response) {
+    return caches.open('offline2').then(function(cache) {
         return cache.put(offlinePage, response);
     });
 });
-self.addEventListener('notificationclick', function (event) {
+self.addEventListener('notificationclick', function(event) {
     var data = event.notification.data;
     event.notification.close();
     event.waitUntil(
@@ -35,6 +35,6 @@ self.addEventListener('push', ev => {
     console.log(data);
     self.registration.showNotification(data.title, {
         body: data.content,
-        icon: '/client/img/MyDustChat.png'
+        icon: '/client/img/Nuclechatroom.png'
     });
 });
